ccaed191603a46fd4cf992433ccd4d24e895130d
imron <muhammadimron39@gmail.com>
2020-02-05 17:40:13 +0700

    first commit

---
+++ src/database/config/config.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+module.exports = {
+	// If using onine database
+	// development: {
+	//   use_env_variable: 'DATABASE_URL'
+	// },
+
+	development: {
+		database: 'nodejs_core_development',
+		username: 'root',
+		password: 'root',
+		host: '127.0.0.1',
+		dialect: 'mysql'
+	},
+
+	test: {
+		database: 'nodejs_core_test',
+		username: 'root',
+		password: 'root',
+		host: '127.0.0.1',
+		dialect: 'mysql'
+	},
+
+	production: {
+		database: process.env.DB_NAME,
+		username: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		host: process.env.DB_HOST,
+		dialect: 'postgres'
+	}
+};
---
+++ src/database/models/book.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+	const Book = sequelize.define(
+		'Book',
+		{
+			title: DataTypes.STRING,
+			price: DataTypes.STRING,
+			description: DataTypes.STRING
+		},
+		{}
+	);
+	Book.associate = function(models) {
+		// associations can be defined here
+	};
+	return Book;
+};
